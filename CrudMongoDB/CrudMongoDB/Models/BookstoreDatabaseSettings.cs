f5b5f592d73776fe821a1e30ace11d535ac21894
marcelomarqueslima <61606942+marcelomarqueslima@users.noreply.github.com>
2020-04-30 22:09:24 -0300

    Crud MongoDB
    Crud MongoDB em .NET Core

---
+++ CrudMongoDB/CrudMongoDB/Models/BookstoreDatabaseSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudMongoDB.Models
+{
+    public class BookstoreDatabaseSettings
+    {
+        public class BookstoreDatabaseSettings : IBookstoreDatabaseSettings
+        {
+            public string BooksCollectionName { get; set; }
+            public string ConnectionString { get; set; }
+            public string DatabaseName { get; set; }
+        }
+    }
+}
